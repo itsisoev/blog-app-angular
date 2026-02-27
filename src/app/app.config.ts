9e4433da9b9363d346744e45d6a1f0e1c206eb92
Daler <isoev9733@gmail.com>
2026-02-27 22:47:41 +0500

    - Установили библиотеку lucide для иконок

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {LUCIDE_ICONS, LucideIconProvider} from 'lucide-angular';
+import {ICONS} from '@shared/constants/icons.constants';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {provide: LUCIDE_ICONS, multi: true, useValue: new LucideIconProvider(ICONS)},
   ]
 };
